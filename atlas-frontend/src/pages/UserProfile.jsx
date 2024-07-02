ce937de07a40c121c535de0bb252798deb07bc6c
Simon Conrad <simondbconrad@gmail.com>
2024-07-02 16:22:12 -0400

    refactor userProfile with rtk

--- atlas-frontend/src/pages/UserProfile.jsx
+++ atlas-frontend/src/pages/UserProfile.jsx
@@ -1,46 +1,16 @@
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const UserProfile = () => {
-    const [profile, setProfile] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
     const user = useSelector((state) => state.user.user);
+    const profile = useSelector((state) => state.profile.profile);
 
-    useEffect(() => {
-        const fetchProfile = async () => {
-            if (user && user.profile) {
-                console.log(user.profile);
-                try {
-                    const response = await fetch(user.profile);
-                    if (response.ok) {
-                        const profileData = await response.json();
-                        setProfile(profileData);
-                    } else {
-                        setError('Failed to fetch profile');
-                    }
-                } catch (error) {
-                    console.error('Error: ', error);
-                } finally {
-                    setLoading(false);
-                }
-            } else {
-                setLoading(false);
-            }
-        };
-        fetchProfile();
-    }, [user]);
-    console.log(profile);
+    // NOTE: update to handle errors, edgecases (profile not accessed gracefully),
+    // loading, etc. This will require an update to profileSlice and profile fetching
+    // logic located in thunks.js.
 
     if (!user) {
         return <div>log in to view your profile</div>;
-    }
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>{error}</div>;
     }
 
     return (
@@ -54,10 +24,21 @@ const UserProfile = () => {
             <div>{profile.image}</div>
             <h3>Friends</h3>
             <div>
-                {profile.friends.map((f) => {
-                    return <p key={f.id}>{f.username}</p>;
+                {profile.friends.map((f, idx) => {
+                    return (
+                        <div key={idx}>
+                            <Link to={`/profile/${f.profile_id}`}>
+                                {f.username}
+                            </Link>
+                            <button>remove</button>
+                        </div>
+                    );
                 })}
             </div>
+            <h3>Beacons</h3>
+            <h5>Past</h5>
+            <h5>Live</h5>
+            <h5>Scheduled</h5>
         </div>
     );
 };
