a05686f5cb350ffb693449d1d0fa42f0b5a29aab
Simon Conrad <simondbconrad@gmail.com>
2024-07-02 11:36:17 -0400

    update app name

--- atlas-frontend/src/pages/Welcome.jsx
+++ atlas-frontend/src/pages/Welcome.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 const Welcome = () => {
     const user = useSelector((state) => state.user.user);
     if (!user) {
-        return <div>Welcome to Atlas Social</div>;
+        return <div>Welcome to Hoppin</div>;
     } else {
         return <div>Hello {user.username}! Welcome to Atlas Social</div>;
     }
