52f36fbab9b1a1c41463c2c26e45bfaf1cb42b61
Simon Conrad <simondbconrad@gmail.com>
2024-07-23 13:29:46 -0400

    create beacon functionality for user

--- atlas-frontend/src/pages/CreateBeaconForm.jsx
+++ atlas-frontend/src/pages/CreateBeaconForm.jsx
@@ -1,24 +1,40 @@
 import { useState, useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
+import getCSRFToken from '../utils/auth';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBeaconForm = () => {
+    //  mapbox initiation
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
     const mapContainer = useRef(null);
     const map = useRef(null);
     const marker = useRef(null);
 
-    //   initialize state values,
+    const navigate = useNavigate();
+
+    //  initialize state values
     const [lng, setLng] = useState(-77.124813);
     const [lat, setLat] = useState(38.92366);
-    const [zoom, setZoom] = useState(12);
+    const zoom = 12; // hard coded value for map rendering... confirm and update this value
     const [mapReady, setMapReady] = useState(false);
+    const [beaconFormData, setBeaconFormData] = useState({
+        description: '',
+        start_datetime: new Date(),
+        end_datetime: new Date(),
+        capacity: 1,
+        location: { lng: null, lat: null },
+        visibility: 'friends',
+        visibility_polygon: null,
+    });
+
+    // ========================= LOCATION SELECTION ===========================
 
     // get user location
     useEffect(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
-                (pos) => {
-                    const { latitude, longitude } = pos.coords;
+                (position) => {
+                    const { latitude, longitude } = position.coords;
                     setLng(longitude);
                     setLat(latitude);
                     setMapReady(true);
@@ -36,7 +52,7 @@ const CreateBeaconForm = () => {
         if (map.current || !mapReady) return;
 
         if (lat && lng) {
-            // create new instance of mapbox map
+            // create new instance of mapbox map, save to map Ref
             map.current = new mapboxgl.Map({
                 container: mapContainer.current,
                 style: 'mapbox://styles/mapbox/streets-v12',
@@ -44,17 +60,7 @@ const CreateBeaconForm = () => {
                 zoom: zoom,
             });
 
-            map.current.on('move', () => {
-                // uses map camera properties
-                const center = map.current.getCenter();
-                const zoom = map.current.getZoom();
-
-                // updates state
-                setLng(center.lng);
-                setLat(center.lat);
-                setZoom(zoom);
-            });
-
+            // click handler for beacon location selection
             map.current.on('click', (e) => {
                 const clickLng = e.lngLat.lng;
                 const clickLat = e.lngLat.lat;
@@ -69,22 +75,15 @@ const CreateBeaconForm = () => {
                     .addTo(map.current);
                 setBeaconFormData((prevData) => ({
                     ...prevData,
-                    location: { longitude: clickLng, latitude: clickLat },
+                    location: { lng: clickLng, lat: clickLat },
                 }));
             });
         }
     }, [lng, lat, zoom, mapReady]);
 
-    const [beaconFormData, setBeaconFormData] = useState({
-        description: '',
-        start_time: new Date(),
-        end_time: new Date(),
-        capacity: 1,
-        location: { lng: null, lat: null },
-        visibility: 'FRIENDS',
-        visibility_polygon: null,
-    });
+    // ========================== FORM HANDLING ===============================
 
+    // handle change in inputs, update beaconFormData
     const handleChange = (event) => {
         const { name, value } = event.target;
         setBeaconFormData((prevData) => ({
@@ -93,10 +92,50 @@ const CreateBeaconForm = () => {
         }));
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(beaconFormData);
+    // format datestring to pass to django
+    const formatDate = (datestring) => {
+        const date = new Date(datestring);
+        return date.toISOString();
     };
+
+    // submit form, 'POST' to users-beacons endpoint
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const csrfToken = getCSRFToken();
+
+        // format data for endpoint
+        const formattedData = {
+            ...beaconFormData,
+            start_datetime: formatDate(beaconFormData.start_datetime),
+            end_datetime: formatDate(beaconFormData.end_datetime),
+            capacity: parseInt(beaconFormData.capacity),
+        };
+
+        // make 'POST' request
+        const fetchUrl = 'http://localhost:8000/api/beacons/mybeacons/';
+        const fetchConfig = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken,
+            },
+            body: JSON.stringify(formattedData),
+            credentials: 'include',
+        };
+        try {
+            const response = await fetch(fetchUrl, fetchConfig);
+            if (response.ok) {
+                // NOTE: add alert behavior for success and failure
+                //  * incomplete field notification
+                //  * server error notification
+                navigate('/myprofile');
+            }
+        } catch (error) {
+            console.error('create beacon failed', error);
+        }
+    };
+
+    // ============================= RENDER PAGE ==============================
 
     return (
         <div>
@@ -114,22 +153,22 @@ const CreateBeaconForm = () => {
                     />
                 </div>
                 <div>
-                    <label htmlFor="start_time">start time</label>
+                    <label htmlFor="start_datetime">start time</label>
                     <input
                         type="datetime-local"
-                        id="start_time"
-                        name="start_time"
-                        value={beaconFormData.start_time}
+                        id="start_datetime"
+                        name="start_datetime"
+                        value={beaconFormData.start_datetime}
                         onChange={handleChange}
                     />
                 </div>
                 <div>
-                    <label htmlFor="end_time">end time</label>
+                    <label htmlFor="end_datetime">end time</label>
                     <input
                         type="datetime-local"
-                        id="end_time"
-                        name="end_time"
-                        value={beaconFormData.end_time}
+                        id="end_datetime"
+                        name="end_datetime"
+                        value={beaconFormData.end_datetime}
                         onChange={handleChange}
                     />
                 </div>
@@ -152,8 +191,8 @@ const CreateBeaconForm = () => {
                         value={beaconFormData.visibility}
                         onChange={handleChange}
                     >
-                        <option value="FRIENDS">visible to friends only</option>
-                        <option value="ALL">visible to all users</option>
+                        <option value="friends">visible to friends only</option>
+                        <option value="all">visible to all users</option>
                     </select>
                 </div>
                 <div>
@@ -168,8 +207,7 @@ const CreateBeaconForm = () => {
                     )}
                 </div>
                 <div>
-                    {beaconFormData.location.longitude},{' '}
-                    {beaconFormData.location.latitude}
+                    {beaconFormData.location.lng}, {beaconFormData.location.lat}
                 </div>
                 <button>create beacon</button>
             </form>
