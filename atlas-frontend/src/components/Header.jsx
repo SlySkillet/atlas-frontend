08899cd8c286101c5ce30c2c0adcf8cf08522e81
Simon Conrad <simondbconrad@gmail.com>
2024-07-29 12:05:36 -0400

    create template for main hamburger menu

--- atlas-frontend/src/components/Header.jsx
+++ atlas-frontend/src/components/Header.jsx
@@ -3,11 +3,14 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { logoutAndClearProfile } from '../features/user/thunks';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from 'react';
 
 const Header = () => {
     const user = useSelector((state) => state.user.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const handleLogout = async (event) => {
         event.preventDefault();
@@ -32,15 +35,52 @@ const Header = () => {
             navigate('/');
         }
     };
+
+    const toggleMenuOpen = () => {
+        setMenuOpen((prevState) => !prevState);
+    };
+    const handleCloseMenu = () => {
+        setMenuOpen(false);
+    };
     return (
-        <div className="container mx-auto flex flex-wrap justify-start md:col-span-2 lg:col-span-3 xl:col-span-4">
+        <div className="container mx-auto flex items-center justify-between p-2 md:col-span-2 lg:col-span-3 xl:col-span-4">
             <h1 className="text-3xl font-bold underline">Hoppin</h1>
             {user ? (
                 <>
                     <p>{user.username}</p>
-                    <button className="button" onClick={handleLogout}>
+                    <GiHamburgerMenu
+                        className="text-xl"
+                        onClick={toggleMenuOpen}
+                    />
+                    <button className="hidden" onClick={handleLogout}>
                         Logout
                     </button>
+                    {menuOpen && (
+                        <div className="absolute inset-0 flex flex-col items-center justify-center gap-4">
+                            <div className="bg-mine-shaft-800 rounded-lg shadow-lg">
+                                <p className="border-b p-4 pl-20 pr-20 text-white">
+                                    Logout
+                                </p>
+                                <p className="border-b p-4 pl-20 pr-20 text-white">
+                                    Edit Profile
+                                </p>
+                                <p className="border-b p-4 pl-20 pr-20 text-white">
+                                    Settings
+                                </p>
+                                <p className="p-4 pl-20 pr-20 text-white">
+                                    Help
+                                </p>
+                            </div>
+                            <div className="bg-mine-shaft-800 rounded-lg shadow-lg">
+                                <p
+                                    className="p-4 pl-20 pr-20 text-white"
+                                    onClick={handleCloseMenu}
+                                >
+                                    Close Menu
+                                </p>
+                            </div>
+                        </div>
+                    )}
                     {/* PREV HEADERS - Links now contained in footer.jsx */}
                     {/* <a className="button" href="/myprofile">
                         my profile
